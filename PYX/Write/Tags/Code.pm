6846361eec18fae0179672cdc89b0e628a32addd
Michal Špaček <skim@cpan.org>
2005-07-13 13:32:09 +0000

    First version.

---
+++ PYX/Write/Tags/Code.pm
@@ -0,0 +1,162 @@
+#------------------------------------------------------------------------------
+package PYX::Write::Tags::Code;
+#------------------------------------------------------------------------------
+# $Id: Code.pm,v 1.1 2005-07-13 13:32:09 skim Exp $
+
+# Pragmas.
+use strict;
+
+# Modules.
+use Carp;
+use PYX::Parser;
+use PYX::Utils qw(encode);
+
+# Version.
+our $VERSION = 0.1;
+
+# Global variables.
+use vars qw(@tag $tag_code);
+
+#------------------------------------------------------------------------------
+sub new {
+#------------------------------------------------------------------------------
+# Constructor.
+
+	my $class = shift;
+	my $self = {};
+	bless $self, $class;
+
+	# Input file handler.
+	$self->{'input_file_handler'} = '';
+
+	# Output handler.
+	$self->{'output_handler'} = *STDOUT;
+
+	# Process params.
+	croak "$class: Created with odd number of parameters - should be ".
+		"of the form option => value." if (@_ % 2);
+	for (my $x = 0; $x <= $#_; $x += 2) {
+		if (exists $self->{$_[$x]}) {
+			$self->{$_[$x]} = $_[$x+1];
+		} else {
+			croak "$class: Bad parameter '$_[$x]'.";
+		}
+	}
+
+	# PYX::Parser object.
+	$self->{'pyx_parser'} = PYX::Parser->new(
+		'input_file_handler' => $self->{'input_file_handler'},
+		'output_handler' => $self->{'output_handler'},
+		'start_tag' => \&_start_tag,
+		'end_tag' => \&_end_tag,
+		'data' => \&_data,
+		'instruction' => \&_instruction,
+		'attribute' => \&_attribute,
+		'comment' => \&_comment,
+	);
+
+	# Tag values.
+	@tag = ();
+
+	# Tag code.
+	$tag_code = $self->{'tag_code'} = [];
+
+	# Object.
+	return $self;
+}
+
+#------------------------------------------------------------------------------
+sub parse {
+#------------------------------------------------------------------------------
+# Start of parsing.
+
+	my $self = shift;
+	$self->{'pyx_parser'}->parse();
+}
+
+#------------------------------------------------------------------------------
+sub get_tags_code {
+#------------------------------------------------------------------------------
+# Gets tags code.
+
+	my $self = shift;
+	return $self->{'tag_code'};
+}
+
+#------------------------------------------------------------------------------
+# Internal methods.
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+sub _start_tag {
+#------------------------------------------------------------------------------
+# Process start of tag.
+
+	shift;
+	my $tag = shift;
+	_flush_tag();
+	push @tag, $tag, [];
+}
+
+#------------------------------------------------------------------------------
+sub _end_tag {
+#------------------------------------------------------------------------------
+# Process end of tag.
+
+	shift;
+	my $tag = shift;
+	_flush_tag();
+	push @{$tag_code}, 'end_'.$tag;
+}
+
+#------------------------------------------------------------------------------
+sub _data {
+#------------------------------------------------------------------------------
+# Process data.
+
+	shift;
+	my $data = encode(shift);
+	_flush_tag();
+	push @{$tag_code}, \$data;
+}
+
+#------------------------------------------------------------------------------
+sub _attribute {
+#------------------------------------------------------------------------------
+# Process attribute.
+
+	shift;
+	push @{$tag[$#tag]}, @_;
+}
+
+#------------------------------------------------------------------------------
+sub _instruction {
+#------------------------------------------------------------------------------
+# Process instruction tag.
+
+	shift;
+	my ($target, $data) = @_;
+}
+
+#------------------------------------------------------------------------------
+sub _flush_tag {
+#------------------------------------------------------------------------------
+# Flush tag values.
+
+	if ($#tag > -1) {
+		push @{$tag_code}, @tag;
+		@tag = ();
+	}
+}
+
+#------------------------------------------------------------------------------
+sub _comment {
+#------------------------------------------------------------------------------
+# Process comments.
+
+	shift;
+	my $comment = encode(shift);
+	# TODO Comment by Tags.
+}
+
+1;
