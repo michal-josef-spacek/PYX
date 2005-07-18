6cad5f727cfefb24ee9512419a6334cac896d45d
Michal Špaček <skim@cpan.org>
2005-07-18 15:58:26 +0000

    First version.

---
+++ PYX/Sort.pm
@@ -0,0 +1,114 @@
+#------------------------------------------------------------------------------
+package PYX::Sort;
+#------------------------------------------------------------------------------
+# $Id: Sort.pm,v 1.1 2005-07-18 15:58:26 skim Exp $
+
+# Pragmas.
+use strict;
+
+# Modules.
+use Carp;
+use PYX;
+use PYX::Parser;
+
+# Version.
+our $VERSION = 0.1;
+
+# Global variables.
+use vars qw($tag);
+
+#------------------------------------------------------------------------------
+sub new {
+#------------------------------------------------------------------------------
+# Constructor.
+
+	my $class = shift;
+	my $self = bless {}, $class;
+
+	# Input file handler.
+	$self->{'input_file_handler'} = '';
+
+	# Output handler.
+	$self->{'output_handler'} = *STDOUT;
+
+	# Process params.
+	croak "$class: Created with odd number of parameters - should be ".
+		"of the form option => value." if (@_ % 2);
+	for (my $x = 0; $x <= $#_; $x += 2) {
+		if (exists $self->{$_[$x]}) {
+			$self->{$_[$x]} = $_[$x+1];
+		} else {
+			croak "$class: Bad parameter '$_[$x]'.";
+		}
+	}
+
+	# PYX::Parser object.
+	$self->{'pyx_parser'} = PYX::Parser->new(
+		'input_file_handler' => $self->{'input_file_handler'},
+		'output_handler' => $self->{'output_handler'},
+		'attribute' => \&_attribute,
+		'start_tag' => \&_tag,
+		'end_tag' => \&_tag,
+		'comment' => \&_tag,
+		'instruction' => \&_tag,
+		'data' => \&_tag,
+	);
+
+	# Tag values.
+	$tag = {};
+
+	# Object.
+	return $self;
+}
+
+#------------------------------------------------------------------------------
+sub parse {
+#------------------------------------------------------------------------------
+# Start of parsing.
+
+	my $self = shift;
+	$self->{'pyx_parser'}->parse();
+}
+
+#------------------------------------------------------------------------------
+# Internal methods.
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+sub _attribute {
+#------------------------------------------------------------------------------
+# Process attribute.
+
+	my $pyx_parser_obj = shift;
+	my $out = $pyx_parser_obj->{'output_handler'};
+	my ($att, $attval) = (shift, shift);
+	$tag->{$att} = $attval;
+}
+
+#------------------------------------------------------------------------------
+sub _tag {
+#------------------------------------------------------------------------------
+# Process tag.
+
+	my $pyx_parser_obj = shift;
+	my $out = $pyx_parser_obj->{'output_handler'};
+	_flush($pyx_parser_obj);
+	print $out $pyx_parser_obj->{'line'}, "\n";
+}
+
+#------------------------------------------------------------------------------
+sub _flush {
+#------------------------------------------------------------------------------
+# Flush attributes.
+
+	my $pyx_parser_obj = shift;
+	my $out = $pyx_parser_obj->{'output_handler'};
+	if (scalar %{$tag}) {
+		foreach my $key (sort keys %{$tag}) {
+			print $out $key.'="'.$tag->{$key}.'"'."\n";
+		}
+		$tag = {};
+	}
+}
+
+1;
