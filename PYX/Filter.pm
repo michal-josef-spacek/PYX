57a16463ff5000f0ce6318f1900f88a43e042f9f
Michal Špaček <skim@cpan.org>
2009-04-13 21:57:04 +0000

    Add doc stub.

--- PYX/Filter.pm
+++ PYX/Filter.pm
@@ -196,3 +196,83 @@ sub _process {
 }
 
 1;
+
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+PYX::Parser - TODO
+
+=head1 SYNOPSIS
+
+TODO
+
+=head1 METHODS
+
+=over 8
+
+=item B<new(%parameters)>
+
+ Constructor.
+
+=over 8
+
+=item * B<output_handler>
+
+TODO
+
+=item * B<rules>
+
+TODO
+
+=back
+
+=item B<parse()>
+
+TODO
+
+=item B<parse_file()>
+
+TODO
+
+=item B<parse_handler()>
+
+TODO
+
+=back
+
+=head1 EXAMPLE
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use PYX::Parser;
+
+ # PYX::Parser object.
+ my $pyx = PYX::Parser->new(
+   TODO
+ );
+
+=head1 DEPENDENCIES
+
+L<Error::Simple::Multiple(3pm)>.
+
+=head1 AUTHOR
+
+Michal Špaček L<skim@skim.cz>.
+
+=head1 LICENSE AND COPYRIGHT
+
+BSD license.
+
+=head1 VERSION
+
+0.01
+
+=cut
