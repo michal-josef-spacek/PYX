be7200bd7dce0066147363e0da13574b0930b384
Michal Špaček <skim@cpan.org>
2009-04-13 22:08:49 +0000

    Rewrite params.

--- PYX/Parser.pm
+++ PYX/Parser.pm
@@ -17,7 +17,7 @@ sub new {
 #------------------------------------------------------------------------------
 # Constructor.
 
-	my $class = shift;
+	my ($class, @params) = @_;
 	my $self = bless {}, $class;
 
 	# Parse handlers.
@@ -38,9 +38,9 @@ sub new {
 	$self->{'output_handler'} = \*STDOUT;
 
 	# Process params.
-        while (@_) {
-                my $key = shift;
-                my $val = shift;
+        while (@params) {
+                my $key = shift @params;
+                my $val = shift @params;
 		if (! exists $self->{$key}) {
 	                err "Unknown parameter '$key'.";
 		}
