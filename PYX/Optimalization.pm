d433d31a62e019138db53844d1f8f67b388dadfa
Michal Špaček <skim@cpan.org>
2009-04-13 21:57:55 +0000

    Rewrite params.

--- PYX/Optimalization.pm
+++ PYX/Optimalization.pm
@@ -20,16 +20,16 @@ sub new {
 #------------------------------------------------------------------------------
 # Constructor.
 
-	my $class = shift;
+	my ($class, @params) = @_;
 	my $self = bless {}, $class;
 
 	# Output handler.
 	$self->{'output_handler'} = \*STDOUT;
 
 	# Process params.
-	while (@_) {
-		my $key = shift;
-		my $val = shift;
+	while (@params) {
+		my $key = shift @params;
+		my $val = shift @params;
 		err "Unknown parameter '$key'." unless exists $self->{$key};
 		$self->{$key} = $val;
 	}
