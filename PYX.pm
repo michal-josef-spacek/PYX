01a97fa8928f52846ffc07bd56f5714db7427ea0
Michal Špaček <tupinek@gmail.com>
2015-09-27 22:54:47 +0200

    Prepare for 0.04 version.

--- PYX.pm
+++ PYX.pm
@@ -14,7 +14,7 @@ Readonly::Array our @EXPORT_OK => qw(attribute char comment end_element instruct
 	start_element);
 
 # Version.
-our $VERSION = 0.04;
+our $VERSION = 0.05;
 
 # Encode attribute as PYX.
 sub attribute {
@@ -187,6 +187,6 @@ L<http://skim.cz>
 
 =head1 VERSION
 
-0.04
+0.05
 
 =cut
--- PYX/Parser.pm
+++ PYX/Parser.pm
@@ -13,7 +13,7 @@ use Readonly;
 Readonly::Scalar my $EMPTY_STR => q{};
 
 # Version.
-our $VERSION = 0.04;
+our $VERSION = 0.05;
 
 # Constructor.
 sub new {
@@ -423,6 +423,6 @@ L<http://skim.cz>
 
 =head1 VERSION
 
-0.04
+0.05
 
 =cut
